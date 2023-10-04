cfcb7d2da6a1ddeb52693b5d17f4a4bcb8240105
Matt Obee <matt@obee.me>
2023-10-04 21:30:14 +0100

    Add recurring dates plugin

--- sanity.config.js
+++ sanity.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {countryStateListPlugin} from 'sanity-plugin-country-state-select'
+import {recurringDates} from 'sanity-plugin-recurring-dates'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
@@ -11,7 +12,7 @@ export default defineConfig({
   projectId: '2g5zqxo3',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool(), countryStateListPlugin()],
+  plugins: [deskTool(), visionTool(), countryStateListPlugin(), recurringDates()],
 
   schema: {
     types: schemaTypes,
