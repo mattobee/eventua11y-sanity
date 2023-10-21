a5c27e85a57926eda33a33944f39c6c01d39deb5
Matt Obee <matt@obee.me>
2023-10-21 12:33:25 +0100

    Create development workspace

--- sanity.config.js
+++ sanity.config.js
@@ -5,16 +5,27 @@ import {countryStateListPlugin} from 'sanity-plugin-country-state-select'
 import {recurringDates} from 'sanity-plugin-recurring-dates'
 import {schemaTypes} from './schemas'
 
-export default defineConfig({
-  name: 'default',
-  title: 'Eventua11y',
-
-  projectId: '2g5zqxo3',
-  dataset: 'production',
-
-  plugins: [deskTool(), visionTool(), countryStateListPlugin(), recurringDates()],
-
-  schema: {
-    types: schemaTypes,
+export default defineConfig([
+  {
+    projectId: '2g5zqxo3',
+    dataset: 'production',
+    name: 'production',
+    basePath: '/production',
+    title: 'Production',
+    plugins: [deskTool(), visionTool(), countryStateListPlugin(), recurringDates()],
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    projectId: '2g5zqxo3',
+    dataset: 'test',
+    name: 'development',
+    basePath: '/test',
+    title: 'Development',
+    plugins: [deskTool(), visionTool(), countryStateListPlugin(), recurringDates()],
+    schema: {
+      types: schemaTypes,
+    },
+  },
+])
