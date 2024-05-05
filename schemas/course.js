ee586a98c252da0fe8beb6828bd066eb445f010a
Matt Obee <matt@obee.me>
2024-05-05 19:06:25 +0100

    Use defineField for all fields

--- schemas/course.js
+++ schemas/course.js
@@ -17,53 +17,53 @@ export const event = defineType({
       type: "text",
       validation: Rule => Rule.required()
     }),
-    {
+    defineField({
         title: 'Cost',
         name: 'cost',
         type: 'string',
         validation: Rule => Rule.required(),
         initialValue: 'paid',
         options: {
-          list: [
-            { title: 'Paid', value: 'paid' },
-            { title: 'Free', value: 'free' }
-          ]
+            list: [
+                { title: 'Paid', value: 'paid' },
+                { title: 'Free', value: 'free' }
+            ]
         }
-      },
-    {
-      title: 'Format',
-      name: 'format',
-      type: 'string',
-      options: {
-        list: [
-          { title: 'Talk', value: 'talk' },
-          { title: 'Workshop', value: 'workshop' },
-          { title: 'Tutorial', value: 'tutorial' },
-          { title: 'Roundtable', value: 'roundtable' },
-          { title: 'Competition', value: 'competition' },
-          { title: 'Panel', value: 'panel' },
-          { title: 'Interview', value: 'interview' },
-          { title: 'Demo', value: 'demo' },
-          { title: 'Keynote', value: 'keynote' },
-          { title: 'Announcement', value: 'announcement' },
-          { title: 'Other', value: 'other' }
-        ]
-      }
-    },
-    {
+    }),
+    defineField({
+        title: 'Format',
+        name: 'format',
+        type: 'string',
+        options: {
+            list: [
+            { title: 'Talk', value: 'talk' },
+            { title: 'Workshop', value: 'workshop' },
+            { title: 'Tutorial', value: 'tutorial' },
+            { title: 'Roundtable', value: 'roundtable' },
+            { title: 'Competition', value: 'competition' },
+            { title: 'Panel', value: 'panel' },
+            { title: 'Interview', value: 'interview' },
+            { title: 'Demo', value: 'demo' },
+            { title: 'Keynote', value: 'keynote' },
+            { title: 'Announcement', value: 'announcement' },
+            { title: 'Other', value: 'other' }
+            ]
+        }
+    }),
+    defineField({
         title: 'Level',
         name: 'level',
         type: 'string',
         validation: Rule => Rule.required(),
         initialValue: 'beginner',
         options: {
-          list: [
-            { title: 'Beginner', value: 'beginner' },
-            { title: 'Intermediate', value: 'intermediate' },
-            { title: 'Advanced', value: 'advanced' }
-          ]
+            list: [
+                { title: 'Beginner', value: 'beginner' },
+                { title: 'Intermediate', value: 'intermediate' },
+                { title: 'Advanced', value: 'advanced' }
+            ]
         }
-      },
+    }),
     defineField({
       title: "Attendance Mode",
       name: "attendanceMode",
