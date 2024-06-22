5f5758da20a808e67972dc6b71a9fab694015261
Matt Obee <matt@obee.me>
2024-06-22 12:07:52 +0100

    Add collection to schematypes

--- schemas/index.js
+++ schemas/index.js
@@ -3,5 +3,6 @@ import event from './event'
 import course from './course'
 import provider from './provider'
 import person from './person'
+import collection from './collection'
 
-export const schemaTypes = [event, course, provider, person]
+export const schemaTypes = [event, course, provider, person, collection]
